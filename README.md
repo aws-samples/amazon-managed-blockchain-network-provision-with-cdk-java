f90be93ccf892e1822caa6cfb59aa48c1d4e6412
Nallamalli <suryajkn@a483e7e3c79f.ant.amazon.com>
2020-11-10 23:58:26 +0530

    Added license file and changed configuration field names

--- README.md
+++ README.md
@@ -1,18 +1,41 @@
-# Welcome to your CDK Java project!
+# Provision Amazon Managed Blockchain Network using CDK and Java Programming Language
 
-This is a blank project for Java development with CDK.
+## Introduction
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+This project provides sample Java code to provision Amazon Managed Blockchain Network using AWS Cloud Development Kit(CDK).
 
-It is a [Maven](https://maven.apache.org/) based project, so you can open this project with any Maven compatible Java IDE to build and run tests.
+Know more details about AWS CDK here https://aws.amazon.com/cdk/
 
-## Useful commands
+Know more details about Amazon Managed Blockchain here https://aws.amazon.com/managed-blockchain
 
- * `mvn package`     compile and run tests
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
+## Prerequisites
 
-Enjoy!
+* Java 8 (v1.8) or later
+* Apache Maven 3.5 or later
+* CDK 1.69 
+* AWS Command Line Interface V2
+
+## How to use this Project
+
+To provision Amazon Blockchain Network using this sample following details have to be provided in AmbNetworkStack.java file.
+
+* NETWORK_NAME
+* NETWORK_DESCRIPTION
+* FRAMEWORK_TYPE
+* FRAMEWORK_VERSION
+* EDITION
+* THRESHOLD_PERCENTAGE
+* THRESHOLD_COMPARATOR
+* PROPOSAL_DURATION
+* MEMBER_NAME
+* MEMBER_DESCRIPTION
+* ADMIN_USERNAME
+* ADMIN_PASSWORD
+
+For description of above fields and allowed values refer https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/AWS_ManagedBlockchain.html 
+
+After providing above values, execute the following commands.
+
+* `cdk synth` this command generates the CloudFormation template under cdk.out folder. Validate the template.
+* `cdk deploy` this command deploys CloudFormation stack
+
--- src/main/java/com/myorg/AmbNetworkStack.java
+++ src/main/java/com/myorg/AmbNetworkStack.java
@@ -1,8 +1,38 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.myorg;
 
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+
+import software.amazon.awscdk.services.managedblockchain.CfnMember;
+import software.amazon.awscdk.services.managedblockchain.CfnNode;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.ApprovalThresholdPolicyProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.MemberConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.MemberFabricConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.MemberFrameworkConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.NetworkConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.NetworkFabricConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.NetworkFrameworkConfigurationProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnMember.VotingPolicyProperty;
+import software.amazon.awscdk.services.managedblockchain.CfnNode.NodeConfigurationProperty;
 
 public class AmbNetworkStack extends Stack {
     public AmbNetworkStack(final Construct scope, final String id) {
@@ -12,6 +42,51 @@ public class AmbNetworkStack extends Stack {
     public AmbNetworkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        final String networkName = "<NETWORK_NAME>";
+        final String networkDescription = "<NETWORK_DESCRIPTION>";
+        final String framework = "<FRAMEWORK_TYPE>";//Allowed value HYPERLEDGER_FABRIC
+        final String frameworkVersion = "<FRAMEWORK_VERSION>";
+        final String edition = "<EDITION>"; //Allowed values are STARTER or STANDARD
+        final long thresholdPercentage = 50; //<THRESHOLD_PERCENTAGE> Number between 0 and 100
+        final String thresholdComparator = "GREATER_THAN"; //<THRESHOLD_COMPARATOR> Allowed values are GREATER_THAN or GREATER_THAN_OR_EQUAL_TO
+        final long proposalDurationInHours = 24; //<PROPOSAL_DURATION> Number between 1 and 168
+
+        final String memberName = "<MEMBER_NAME>";
+        final String memberDescription = "<MEMBER_DESCRIPTION>";
+        final String memberAdminUsername = "<ADMIN_USERNAME>";
+        final String memberAdminPassword = "<ADMIN_PASSWORD>!";
+        
+        final String peerNodeAvailabilityZone = "<AVAILABILITY_ZONE>"; //Availability Zone e.g. us-east-1a
+        final String peerNodeInstanceType = "<INSTANCE_TYPE>"; //Instance Type bc.t3.small
+
+
         // The code that defines your stack goes here
+        NetworkConfigurationProperty networkConfiguration = NetworkConfigurationProperty.builder()
+                .name(networkName).description(networkDescription).frameworkVersion(frameworkVersion)
+                .framework(framework)
+                .networkFrameworkConfiguration(NetworkFrameworkConfigurationProperty.builder()
+                        .networkFabricConfiguration(
+                                NetworkFabricConfigurationProperty.builder().edition(edition).build())
+                        .build())
+                .votingPolicy(VotingPolicyProperty.builder()
+                        .approvalThresholdPolicy(ApprovalThresholdPolicyProperty.builder().proposalDurationInHours(proposalDurationInHours)
+                                .thresholdComparator(thresholdComparator).thresholdPercentage(thresholdPercentage).build())
+                        .build())
+                .build();
+        MemberConfigurationProperty memberConfiguration = MemberConfigurationProperty.builder().name(memberName)
+                .description(memberDescription)
+                .memberFrameworkConfiguration(MemberFrameworkConfigurationProperty.builder()
+                        .memberFabricConfiguration(MemberFabricConfigurationProperty.builder()
+                                .adminUsername(memberAdminUsername).adminPassword(memberAdminPassword).build())
+                        .build())
+                .build();
+        CfnMember ambMeber = CfnMember.Builder.create(this, "MyFirstAMBNetwork")
+                .networkConfiguration(networkConfiguration).memberConfiguration(memberConfiguration).build();
+
+        NodeConfigurationProperty nodeConfiguration = NodeConfigurationProperty.builder().availabilityZone(peerNodeAvailabilityZone)
+                .instanceType(peerNodeInstanceType).build();
+
+        CfnNode ambMemberNode = CfnNode.Builder.create(this, "FirstAMBMemberNode").networkId(ambMeber.getAttrNetworkId())
+                .memberId(ambMeber.getAttrMemberId()).nodeConfiguration(nodeConfiguration).build();
     }
 }
